35f290230e147091553a478259857f068e0ca38e
student <1>
2021-09-15 13:49:29 +0300

    1

---
+++ regestration.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Форма регистрации</title>
+</head>
+<body>
+<h1>
+    Регистрация:
+</h1>
+<form action="regestration_action.php" method="get">
+    Имя пользователя: <input type="text" name="name"/><br />
+    Email: <input type="text" name="email"/><br />
+    Пароль: <input type="password" name="pword"/><br />
+    <input type="submit" value="Отправить">
+</form>
+</body>
+</html>
